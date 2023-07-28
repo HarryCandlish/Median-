7eafac564a75f27ac416901651f3c666cc8335ce
Harry Candlish <harry@Natalies-MacBook-Pro.local>
2023-07-28 11:42:53 +0100

    added sort by functionality ffor the search results page, the results can now be sorted by both relevance and the date uploaded

--- src/data/NewsPosts.js
+++ src/data/NewsPosts.js
@@ -6,7 +6,8 @@ export const NewsPosts = [
         category: 'Ukraine',
         image: '../offensive.jpg',
         priority: 1,
-        content: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."'
+        content: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
+        dateUploaded: '2023-05-26T15:30:00'
     },
     {
         id:2,
@@ -15,7 +16,8 @@ export const NewsPosts = [
         category: 'China',
         image: '../china.jpg',
         priority: 2,
-        content: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."'
+        content: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
+        dateUploaded: '2023-06-26T15:30:00'
     },
     {
         id:3,
@@ -24,7 +26,19 @@ export const NewsPosts = [
         category: 'Sudan',
         image: '../china.jpg',
         priority: 2,
-        content: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."'
+        content: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
+        dateUploaded: '2023-07-26T15:30:00'
+    },
+    {
+        id:4,
+        title: 'Vietnam',
+        description: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
+        category: 'china',
+        image: '../ukraine.jpg',
+        priority: 2,
+        content: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."',
+        dateUploaded: '2023-07-26T15:30:00'
     }
+
 ]
 
--- src/pages/SearchResults.jsx
+++ src/pages/SearchResults.jsx
@@ -1,25 +1,68 @@
-// SearchResults.js
-import React from 'react';
+import React, { useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
-import {NewsPosts} from '../data/NewsPosts'; 
-import Search from '../components/Search'
+import {NewsPosts} from '../data/NewsPosts'; // Import the data from the data.js file
 
 const SearchResults = () => {
   const [searchParams] = useSearchParams();
   const queryParameter = searchParams.get('q');
 
-  // Filter the data based on the search query
+  // State variable for sorting option
+  const [sortingOption, setSortingOption] = useState('relevance');
+
+  // Filter the data based on the search query (same filtering logic as before)
   const filteredData = NewsPosts.filter((item) =>
-  item.title?.toLowerCase().includes(queryParameter?.toLowerCase() || '')
+    item.title?.toLowerCase().includes(queryParameter?.toLowerCase() || '') ||
+    item.category?.toLowerCase().includes(queryParameter?.toLocaleLowerCase() || '')
   );
+
+  // Function to sort the data based on the selected option
+  const sortData = () => {
+    if (sortingOption === 'relevance') {
+      // Sort by relevance (no specific sorting needed for relevance)
+      const sortedByRelevance = [...filteredData].sort((a, b) => {
+        const aCategoryMatch = a.category?.toLowerCase().includes(queryParameter?.toLowerCase());
+        const bCategoryMatch = b.category?.toLowerCase().includes(queryParameter?.toLowerCase());
+
+        if (aCategoryMatch && !bCategoryMatch) return -1;
+        if (!aCategoryMatch && bCategoryMatch) return 1;
+      });
+      return sortedByRelevance;
+    }
+    else if (sortingOption === 'dateUploaded') {
+      // Sort by date uploaded (if your data has a property for date uploaded)
+      const sortedByDate = [...filteredData].sort((a, b) => {
+        // Replace 'dateUploaded' with the actual property title for date uploaded
+        return new Date(b.dateUploaded) - new Date(a.dateUploaded);
+      });
+      return sortedByDate;
+    } else {
+      // Default: return the original filtered data
+      return filteredData;
+    }
+  };
+
+  const sortedData = sortData();
 
   return (
     <div>
-    <Search/>
+      {/* Add the "Sort By" dropdown */}
+      <div>
+        <label htmlFor="sortOption">Sort By:</label>
+        <select
+          id="sortOption"
+          value={sortingOption}
+          onChange={(e) => setSortingOption(e.target.value)}
+        >
+          <option value="relevance">Relevance</option>
+          <option value="dateUploaded">Date Uploaded</option>
+        </select>
+      </div>
+
+      {/* Display the sorted search results */}
       <h1>Search Results for: {queryParameter}</h1>
-      {filteredData.length > 0 ? (
+      {sortedData.length > 0 ? (
         <ul>
-          {filteredData.map((item) => (
+          {sortedData.map((item) => (
             <li key={item.id}>
               <h3>{item.title}</h3>
               <p>{item.description}</p>
